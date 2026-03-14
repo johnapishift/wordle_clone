f30c69507344a27d2b0f411c7abb3179b0ed237a
johnapishift <johnapishift@gmail.com>
2026-03-14 18:52:28 +1300

    fix 2

--- app/src/wordList.ts
+++ app/src/wordList.ts
@@ -1,5 +1,1 @@
-import raw from '../../webapp/data/languages/en/en_5words.txt?raw'
-
-export const validWords = new Set(
-    raw.split('\n').map((w: string) => w.trim().toLowerCase()).filter((w: string) => w.length === 5),
-)
+export { validWords } from './wordList.generated'
